7cd9769a0d6ff5a5deab6067b4cf0dacbb1f417c
Gegham Zakaryan <zakaryan.2004@outlook.com>
2021-01-08 16:05:45 +0400

    helper_funcs/filters: Use MessageFilter instead of BaseFilter
    In ptb13, MessageFilter or UpdateFilter should be used as a base for custom filters, as they have been refactored.
    
    Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>

--- haruka/modules/helper_funcs/filters.py
+++ haruka/modules/helper_funcs/filters.py
@@ -16,20 +16,20 @@
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 from telegram import Message
-from telegram.ext import BaseFilter
+from telegram.ext import MessageFilter
 
 from haruka import SUDO_USERS
 
 
 class CustomFilters(object):
-    class _Sudoers(BaseFilter):
+    class _Sudoers(MessageFilter):
         def filter(self, message: Message):
             return bool(message.from_user
                         and message.from_user.id in SUDO_USERS)
 
     sudo_filter = _Sudoers()
 
-    class _MimeType(BaseFilter):
+    class _MimeType(MessageFilter):
         def __init__(self, mimetype):
             self.mime_type = mimetype
             self.name = "CustomFilters.mime_type({})".format(self.mime_type)
@@ -40,7 +40,7 @@ class CustomFilters(object):
 
     mime_type = _MimeType
 
-    class _HasText(BaseFilter):
+    class _HasText(MessageFilter):
         def filter(self, message: Message):
             return bool(message.text or message.sticker or message.photo
                         or message.document or message.video)
