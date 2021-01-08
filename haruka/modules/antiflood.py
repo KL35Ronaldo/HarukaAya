736655f26063130bb30727fc3333438344331989
Gegham Zakaryan <zakaryan.2004@outlook.com>
2021-01-08 22:13:19 +0400

    antiflood: Migrate to context-based handlers, use ChatPermissions
    Antiflood now uses the new ChatPermissions object for setting the restricted permissions after flood.
    
    Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>

--- haruka/modules/antiflood.py
+++ haruka/modules/antiflood.py
@@ -18,9 +18,10 @@
 import html
 from typing import List
 
-from telegram import Update, Bot
+from telegram import Update, ChatPermissions
 from telegram.error import BadRequest
-from telegram.ext import Filters, MessageHandler, CommandHandler, run_async
+from telegram.ext import Filters, MessageHandler, CommandHandler
+from telegram.ext.callbackcontext import CallbackContext
 from telegram.utils.helpers import mention_html
 
 from haruka import dispatcher
@@ -33,9 +34,8 @@ from haruka.modules.tr_engine.strings import tld
 FLOOD_GROUP = 3
 
 
-@run_async
 @loggable
-def check_flood(bot: Bot, update: Update) -> str:
+def check_flood(update: Update, context: CallbackContext) -> str:
     user = update.effective_user
     chat = update.effective_chat
     msg = update.effective_message
@@ -53,7 +53,7 @@ def check_flood(bot: Bot, update: Update) -> str:
         return ""
 
     try:
-        bot.restrict_chat_member(chat.id, user.id, can_send_messages=False)
+        context.bot.restrict_chat_member(chat.id, user.id, ChatPermissions(can_send_messages=False))
         msg.reply_text(tld(chat.id, "flood_mute"))
 
         return tld(chat.id, "flood_logger_success").format(
@@ -65,11 +65,11 @@ def check_flood(bot: Bot, update: Update) -> str:
         return tld(chat.id, "flood_logger_fail").format(chat.title)
 
 
-@run_async
 @user_admin
 @can_restrict
 @loggable
-def set_flood(bot: Bot, update: Update, args: List[str]) -> str:
+def set_flood(update: Update, context: CallbackContext) -> str:
+    args = context.args
     chat = update.effective_chat
     user = update.effective_user
     message = update.effective_message
@@ -106,8 +106,7 @@ def set_flood(bot: Bot, update: Update, args: List[str]) -> str:
     return ""
 
 
-@run_async
-def flood(bot: Bot, update: Update):
+def flood(update: Update, context: CallbackContext):
     chat = update.effective_chat
 
     limit = sql.get_flood_limit(chat.id)
@@ -127,12 +126,13 @@ __help__ = True
 # TODO: Add actions: ban/kick/mute/tban/tmute
 
 FLOOD_BAN_HANDLER = MessageHandler(
-    Filters.all & ~Filters.status_update & Filters.group, check_flood)
+    Filters.all & ~Filters.status_update & Filters.chat_type.groups, check_flood, run_async=True)
 SET_FLOOD_HANDLER = CommandHandler("setflood",
                                    set_flood,
                                    pass_args=True,
-                                   filters=Filters.group)
-FLOOD_HANDLER = CommandHandler("flood", flood, filters=Filters.group)
+                                   run_async=True,
+                                   filters=Filters.chat_type.groups)
+FLOOD_HANDLER = CommandHandler("flood", flood, run_async=True, filters=Filters.chat_type.groups)
 
 dispatcher.add_handler(FLOOD_BAN_HANDLER, FLOOD_GROUP)
 dispatcher.add_handler(SET_FLOOD_HANDLER)
